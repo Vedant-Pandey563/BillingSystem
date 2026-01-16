8420ba139a4307474b768265f18c155d439f2542
Vedant Pandey <ved2003ant@gmail.com>
2026-01-16 14:14:53 +0530

    Add tax calculation on feature

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
+
 using System;
 
 class Program
 {
     static void Main()
     {
+        int amount = 500;
+        int tax = 50;
+
         Console.WriteLine("Billing System");
-        Console.WriteLine("Total Amount: 500");
+        Console.WriteLine("Amount: " + amount);
+        Console.WriteLine("Tax: " + tax);
+        Console.WriteLine("Total: " + (amount + tax));
     }
 }
